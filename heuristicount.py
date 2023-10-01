f3335dd7552d2d8dd96a50f8c5a5394f5e68dbcc
Ryan Valverde de Ward <ryandward@gmail.com>
2023-10-01 17:25:18 -0500

    fix: length of zero junction

--- heuristicount.py
+++ heuristicount.py
@@ -75,89 +75,115 @@ def read_fastq(fastq1_file, fastq2_file, num_threads):
         chunk_size = len(reads1) // num_threads
         return [(reads1[i:i+chunk_size], None) for i in range(0, len(reads1), chunk_size)]
 
+def count_barcodes(seq, barcode_length, barcodes):
+    # Generate k-mers and count matches with barcodes
+    kmers = {seq[i:i + barcode_length] for i in range(len(seq) - barcode_length + 1)}
+    return len(barcodes & kmers)
+
 def determine_forward_read(sample1, sample2, barcodes):
-    count1, count2 = 0, 0
     barcode_length = len(next(iter(barcodes)))
+    count1, count2 = 0, 0
 
-    if sample2 is None:  # For single-end reads
-        for seq in sample1[:250]:
-            kmers = {seq[i:i + barcode_length] for i in range(len(seq) - barcode_length + 1)}
-            if barcodes & kmers:
-                count1 += 1
+    for i in range(min(250, len(sample1))):
+        seq1 = sample1[i]
+        seq2 = sample2[i] if sample2 else None  # seq2 is None for single-end
+
+        # Count barcode matches in seq1
+        count1 += count_barcodes(seq1, barcode_length, barcodes)
+        
+        # Early exit if count1 is overwhelmingly large
+        if count1 > 125:
+            return False
+        
+        # For paired-end or reverse complement of single-end
+        if seq2:
+            # Count barcode matches in seq2
+            count2 += count_barcodes(seq2, barcode_length, barcodes)
             
-            # Check the reverse complement
-            seq_rc = seq[::-1].translate(str.maketrans("ATCGN", "TAGCN"))
-            kmers_rc = {seq_rc[i:i + barcode_length] for i in range(len(seq_rc) - barcode_length + 1)}
-            if barcodes & kmers_rc:
-                count2 += 1
+            # Early exit if count2 is overwhelmingly large
+            if count2 > 125:
+                return True
+        else:
+            # Count barcode matches in reverse complement of seq1
+            seq1_rc = seq1[::-1].translate(str.maketrans("ATCGN", "TAGCN"))
+            count2 += count_barcodes(seq1_rc, barcode_length, barcodes)
+            
+            # Early exit if count2 is overwhelmingly large
+            if count2 > 125:
+                return True
 
-        return count2 > count1  # True if more barcodes found in the reverse complement
-
-    else:  # For paired-end reads
-        for seq1, seq2 in zip(sample1[:250], sample2[:250]):
-            kmers1 = {seq1[i:i + barcode_length] for i in range(len(seq1) - barcode_length + 1)}
-            kmers2 = {seq2[i:i + barcode_length] for i in range(len(seq2) - barcode_length + 1)}
-
-            if barcodes & kmers1:
-                count1 += 1
-            if barcodes & kmers2:
-                count2 += 1
-
-        return count2 > count1  # True if more barcodes found in sample2, implying need for swap
-
-from collections import Counter
+    # True if more barcodes found in seq2 or reverse complement of seq1
+    return count2 > count1
 
 
-def find_ends(reads, start, length):
-    lefts, rights = [], []
+def find_ends(reads, start, length, reverse_strand=False):
+    # Initialize max values; reverse if needed
+    max_left, max_right = (4, 4) if not reverse_strand else (4, 4)[::-1]
+
+    # Initialize Counters for left and right flanking regions
+    lefts, rights = Counter(), Counter()
 
     for read in reads[:250]:
-        left = read[start - 4: start]
-        right = read[start + length: start + length + 4]
-        lefts.append(left)
-        rights.append(right)
+        max_left = min(max_left, start)
+        max_right = min(max_right, len(read) - (start + length))
 
-    left = Counter(lefts).most_common(1)[0][0]
-    right = Counter(rights).most_common(1)[0][0]
-    return left, right
+        # Extract flanking sequences
+        left = read[start - max_left: start]
+        right = read[start + length: start + length + max_right]
 
-from collections import defaultdict
+        # Update Counters
+        if left:
+            lefts[left] += 1
+        if right:
+            rights[right] += 1
 
+    # Find the most common flanking sequence
+    left_most_common = lefts.most_common(1)[0][0] if lefts else None
+    right_most_common = rights.most_common(1)[0][0] if rights else None
 
-def process_chunk(chunk, barcodes, barcode_start1=None, barcode_start2=None, barcode_length=None):
+    return left_most_common, right_most_common
+
+def process_chunk(chunk, barcodes, barcode_start1=None, barcode_start2=None, barcode_length=None, left1=None, right1=None, left2=None, right2=None):
     reads1, reads2 = chunk
     counts = defaultdict(int)
-    unexpected_sequences = defaultdict(int)
 
     if reads1 and reads2:  # Paired-end processing
         for rec1, rec2 in zip(reads1, reads2):
             candidate1 = rec1[barcode_start1:barcode_start1 + barcode_length]
             candidate2 = rec2[barcode_start2:barcode_start2 + barcode_length]
             candidate2_rc = candidate2[::-1].translate(str.maketrans("ATCGN", "TAGCN"))
-
-            if candidate1 in barcodes and candidate2_rc == candidate1:
-                counts[candidate1[4:-4]] += 1
-            elif candidate1 not in barcodes and candidate2_rc == candidate1:
-                unexpected_sequences[candidate1] += 1
+            if candidate2_rc == candidate1:
+                if candidate1 in barcodes:
+                    counts[candidate1[len(left1):-len(right1) or None]] += 1
+                else:
+                    if candidate1.startswith(left1) and candidate1.endswith(right1):
+                        barcode = candidate1[len(left1):-len(right1) or None] + "*"  
+                        counts[barcode] += 1
 
     elif reads1:  # Single-end processing, forward orientation
         for rec1 in reads1:
             candidate1 = rec1[barcode_start1:barcode_start1 + barcode_length]
             if candidate1 in barcodes:
-                counts[candidate1[4:-4]] += 1
+                counts[candidate1[len(left1):-len(right1) or None]] += 1
             else:
-                unexpected_sequences[candidate1] += 1
+                if candidate1.startswith(left1) and candidate1.endswith(right1):
+                    barcode = candidate1[len(left1):-len(right1) or None] + "*"  
+                    counts[barcode] += 1
 
     elif reads2:  # Single-end processing, reverse complement
         for rec2 in reads2:
             candidate2 = rec2[barcode_start2:barcode_start2 + barcode_length]
             candidate2_rc = candidate2[::-1].translate(str.maketrans("ATCGN", "TAGCN"))
             if candidate2 in barcodes:
-                counts[candidate2_rc[4:-4]] += 1
+                counts[candidate2_rc[len(right2):-len(left2) or None]] += 1
             else:
-                unexpected_sequences[candidate2_rc] += 1
+                # print(candidate2, left2, right2, file=sys.stderr)
+                if candidate2.startswith(left2) and candidate2.endswith(right2):
+                    barcode = candidate2_rc[len(right2):-len(left2) or None] + "*"  
+                    counts[barcode] += 1
+    
+    return counts
 
-    return counts, unexpected_sequences
 
 def find_start_positions(reads, barcodes, barcode_length, is_read2=False):
     if reads is None:
@@ -241,16 +267,45 @@ def main(args):
     if sample1 is not None:
         console.log("Finding forward junctions...")
         left1, right1 = find_ends(sample1, barcode_start1, barcode_length)
-        barcode_start1 -= len(left1)
+        barcode_start1 -= len(left1) if left1 else 0
     else:
         left1, right1 = None, None
 
-    if sample2:
+    if sample2 is not None:
         console.log("Finding reverse junctions...")
-        left2, right2 = find_ends(sample2, barcode_start2, barcode_length)
-        barcode_start2 -= len(left2)
+        left2, right2 = find_ends(sample2, barcode_start2, barcode_length, reverse_strand=True)
+        barcode_start2 -= len(left2) if left2 else 0
     else:
         left2, right2 = None, None
+
+   # Calculate reverse complements
+    left2_rc = left2[::-1].translate(str.maketrans("ATCGN", "TAGCN")) if left2 else None
+    right2_rc = right2[::-1].translate(str.maketrans("ATCGN", "TAGCN")) if right2 else None
+
+
+    # Check if the forward and reverse flanking sequences are reverse complements
+    min_len_left = min_len_right = 0
+
+    if left1 and right2_rc:
+        min_len_left = min(len(left1), len(right2_rc))
+        console.log(f"Comparing {left1[-min_len_left:]} and {right2_rc[:min_len_left]}")
+        if left1[-min_len_left:] != right2_rc[:min_len_left]:
+
+            console.log("[bold red]Error: Forward and reverse left flanking sequences are not reverse complements.[/bold red]")
+            sys.exit(1)
+        
+
+    if right1 and left2_rc:
+        min_len_right = min(len(right1), len(left2_rc))
+        console.log(f"Comparing {right1[:min_len_right]} and {left2_rc[:min_len_right]}")
+        if right1[:min_len_right] != left2_rc[:min_len_right]:
+
+            console.log("[bold red]Error: Forward and reverse right flanking sequences are not reverse complements.[/bold red]")
+            sys.exit(1)
+
+        min_junction_len = min(min_len_left, min_len_right) 
+        left1, right1 = left1[-min_junction_len:], right1[:min_junction_len] if left1 and right1 else None
+        left2, right2 = left2[-min_junction_len:], right2[:min_junction_len] if left2 and right2 else None
 
     # Update barcodes
     console.log("Mapping junctions to barcodes...")
@@ -260,37 +315,25 @@ def main(args):
         barcodes = {left2 + barcode[::-1].translate(str.maketrans("ATCGN", "TAGCN")) + right2 for barcode in barcodes}
     barcode_length = len(next(iter(barcodes)))
 
-    # Processing Chunks
+     # Processing Chunks
     console.log("Counting barcodes at junctions...")
     with Pool(num_threads) as pool:
-        results = pool.starmap(process_chunk, [(chunk, barcodes, barcode_start1, barcode_start2, barcode_length) for chunk in chunks])
+        results = pool.starmap(process_chunk, [(chunk, barcodes, barcode_start1, barcode_start2, barcode_length, left1, right1, left2, right2) for chunk in chunks])
+
 
     # Collating Results
     console.log("Preparing results...")
-    final_counts = Counter()
-    final_unexpected_sequences = Counter()
-    for counts, unexpected_sequences in results:
-        final_counts.update(counts)
-        final_unexpected_sequences.update(unexpected_sequences)
+    
+    documented_barcodes = Counter()
+    undocumented_barcodes = Counter()
 
-    # Filtering and Creating New Barcodes
-    console.log("Heuristic filtering and identifying new barcodes...")
-    new_barcodes = Counter()
-    left, right = (left1, right1) if left1 is not None else (left2, right2)
-    if left1 is None and left2 is not None:
-        left, right = right[::-1].translate(str.maketrans("ATCGN", "TAGCN")), left[::-1].translate(str.maketrans("ATCGN", "TAGCN"))
+    for result in results:
+        for barcode, count in result.items():
+            if barcode.endswith('*'):
+                undocumented_barcodes[barcode] += count
+            else:
+                documented_barcodes[barcode] += count
 
-
-    # Initialize new_barcodes
-    new_barcodes = Counter()
-
-    if left and right:  # Check if either pair is not None
-        for seq, count in final_unexpected_sequences.items():
-            if seq.startswith(left) and seq.endswith(right):
-                barcode = seq[4:-4] + "*"                 
-                new_barcodes[barcode] = count
-
-    # final_counts = final_counts + new_barcodes
 
     total_reads = sum(len(chunk[0]) if chunk[0] is not None else len(chunk[1]) for chunk in chunks)
 
@@ -337,15 +380,18 @@ def main(args):
     combined_table.add_row("[bold bright_blue]Heuristics[/bold bright_blue]", "")
 
     if barcode_length:
-        combined_table.add_row("Barcode Length", f"[bold]{barcode_length-8}[/bold]")
+        len_left = len(left1) if left1 else len(right2) if right2 else 0
+        len_right = len(right1) if right1 else len(left2) if left2 else None
+        combined_table.add_row("Barcode Length", f"[bold]{barcode_length - len_left - (len_right if len_right is not None else 0)}[/bold]")
     if barcode_start1:
-        combined_table.add_row("Forward Offset", f"[bold]{barcode_start1 + 4}[/bold]")
+        combined_table.add_row("Forward Offset", f"[bold]{barcode_start1 + len(left1)}[/bold]")
     if barcode_start2:
-        combined_table.add_row("Reverse Offset", f"[bold]{barcode_start2 + 4}[/bold]")
+        combined_table.add_row("Reverse Offset", f"[bold]{barcode_start2 + len(right2)}[/bold]")
     if left1 and right1:
         combined_table.add_row("Forward Junction", f"[bold]{left1}...{right1}[/bold]")
     if left2 and right2:
         combined_table.add_row("Reverse Junction", f"[bold]{left2}...{right2}[/bold]")
+
 
     # Numeric Statistics Sub-heading
     combined_table.add_section()
@@ -353,40 +399,40 @@ def main(args):
 
     # Rows for Numeric Statistics
     combined_table.add_row("Barcodes Declared", f"[bold]{len(barcodes)}[/bold]")
-    combined_table.add_row("Documented Barcodes Found", f"[bold]{len(final_counts)}[/bold]")
-    combined_table.add_row("Undocumented Barcodes Found", f"[bold]{len(new_barcodes)}[/bold]")
+    combined_table.add_row("Documented Barcodes Found", f"[bold]{len(documented_barcodes)}[/bold]")
+    combined_table.add_row("Undocumented Barcodes Found", f"[bold]{len(undocumented_barcodes)}[/bold]")
     combined_table.add_row("Total Reads", f"[bold]{total_reads}[/bold]")
-    combined_table.add_row("Documented Barcode Reads", f"[bold]{sum(final_counts.values())}[/bold]")
-    combined_table.add_row("Undocumented Barcode Reads", f"[bold]{sum(new_barcodes.values())}[/bold]")
-    combined_table.add_row("Documented Fraction", f"[bold]{(sum(final_counts.values()) / total_reads if total_reads != 0 else 0):.4f}[/bold]")
-    combined_table.add_row("Undocumented Fraction", f"[bold]{(sum(new_barcodes.values()) / total_reads if total_reads != 0 else 0):.4f}[/bold]", end_section=True)
+    combined_table.add_row("Documented Barcode Reads", f"[bold]{sum(documented_barcodes.values())}[/bold]")
+    combined_table.add_row("Undocumented Barcode Reads", f"[bold]{sum(undocumented_barcodes.values())}[/bold]")
+    combined_table.add_row("Documented Fraction", f"[bold]{(sum(documented_barcodes.values()) / total_reads if total_reads != 0 else 0):.4f}[/bold]")
+    combined_table.add_row("Undocumented Fraction", f"[bold]{(sum(undocumented_barcodes.values()) / total_reads if total_reads != 0 else 0):.4f}[/bold]", end_section=True)
 
     # Sequence Information Sub-heading  
     combined_table.add_section()
 
-    # Add final_counts to the main table
+    # Add documented_barcodes to the main table
     combined_table.add_section()
-    top_final_counts = min(5, len(final_counts))
-    combined_table.add_row(f"[bold bright_yellow]Top {top_final_counts} Documented Barcodes[/bold bright_yellow]", "")
-    for idx, (barcode, count) in enumerate(final_counts.most_common(top_final_counts)):
-        end_section = idx == (top_final_counts - 1)
+    top_documented_barcodes = min(5, len(documented_barcodes))
+    combined_table.add_row(f"[bold bright_yellow]Top {top_documented_barcodes} Documented Barcodes[/bold bright_yellow]", "")
+    for idx, (barcode, count) in enumerate(documented_barcodes.most_common(top_documented_barcodes)):
+        end_section = idx == (top_documented_barcodes - 1)
         combined_table.add_row(barcode, str(count), end_section=end_section)
 
-    # Add new_barcodes to the main table
+    # Add undocumented_barcodes to the main table
     combined_table.add_section()
-    top_new_barcodes = min(5, len(new_barcodes))
-    combined_table.add_row(f"[bold bright_red]Top {top_new_barcodes} Undocumented Barcodes[/bold bright_red]", "")
-    for idx, (barcode, count) in enumerate(new_barcodes.most_common(top_new_barcodes)):
-        end_section = idx == (top_new_barcodes - 1)
+    top_undocumented_barcodes = min(5, len(undocumented_barcodes))
+    combined_table.add_row(f"[bold bright_red]Top {top_undocumented_barcodes} Undocumented Barcodes[/bold bright_red]", "")
+    for idx, (barcode, count) in enumerate(undocumented_barcodes.most_common(top_undocumented_barcodes)):
+        end_section = idx == (top_undocumented_barcodes - 1)
         combined_table.add_row(barcode, str(count), end_section=end_section)
 
     # Print the combined table
     console.log(combined_table)
 
-    for barcode, count in final_counts.items():
+    for barcode, count in documented_barcodes.items():
         print("\t".join([barcode, str(count)]))
 
-    for barcode, count in new_barcodes.items():
+    for barcode, count in undocumented_barcodes.items():
         print("\t".join([barcode, str(count)]))
 
 if __name__ == "__main__":
