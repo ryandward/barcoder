9d42f3e6f9ca19001448e4709a57dfbb0760e4d4
Ryan Valverde de Ward <ryandward@gmail.com>
2023-10-02 13:24:25 -0500

    add: arbitrary number of files to sort

--- distillreads.py
+++ distillreads.py
@@ -3,9 +3,11 @@ import sys
 from concurrent.futures import ThreadPoolExecutor
 import pyzstd
 
+
 def compress_and_write(output_file):
     with open(output_file, 'rb') as f_in, pyzstd.open(output_file + '.zst', 'wb') as f_out:
         f_out.write(f_in.read())
+
 
 def parallel_sort(paired_reads):
     chunk_size = len(paired_reads) // 4
@@ -16,69 +18,71 @@ def parallel_sort(paired_reads):
         
     return sorted_chunks
 
+
 def merge_sorted_chunks(sorted_chunks):
     from heapq import merge
     return list(merge(*sorted_chunks))
 
-def read_and_process(file1, file2):
+
+def read_and_process(*files):
     sampled_paired_reads = []
     remaining_paired_reads = []
-    
-    with gzip.open(file1, 'rt') as f1, gzip.open(file2, 'rt') as f2:
-        for i in range(1000):
-            f1.readline()
-            seq1 = f1.readline().strip()
-            f1.readline()
-            f1.readline()
 
-            f2.readline()
-            seq2 = f2.readline().strip()
-            f2.readline()
-            f2.readline()
-            
-            sampled_paired_reads.append((seq1, seq2))
+    file_handles = [gzip.open(file, 'rt') for file in files]
+    
+    for _ in range(1000):
+        read_tuple = []
+        for f in file_handles:
+            f.readline()
+            seq = f.readline().strip()
+            f.readline()
+            f.readline()
+            read_tuple.append(seq)
         
-        while True:
-            f1.readline()
-            seq1 = f1.readline().strip()
-            f1.readline()
-            f1.readline()
-
-            f2.readline()
-            seq2 = f2.readline().strip()
-            f2.readline()
-            f2.readline()
-            
-            if not seq1:
-                break
-                
-            remaining_paired_reads.append((seq1, seq2))
+        sampled_paired_reads.append(tuple(read_tuple))
     
+    while True:
+        read_tuple = []
+        for f in file_handles:
+            f.readline()
+            seq = f.readline().strip()
+            f.readline()
+            f.readline()
+            read_tuple.append(seq)
+
+        if not all(read_tuple):
+            break
+
+        remaining_paired_reads.append(tuple(read_tuple))
+
+    for f in file_handles:
+        f.close()
+        
     sorted_chunks = parallel_sort(remaining_paired_reads)
     sorted_remaining_paired_reads = merge_sorted_chunks(sorted_chunks)
-    
+
     final_paired_reads = sampled_paired_reads + sorted_remaining_paired_reads
-    
-    output_file1 = file1.replace(".fastq.gz", ".reads")
-    output_file2 = file2.replace(".fastq.gz", ".reads")
-    
-    with open(output_file1, "wb") as out1, open(output_file2, "wb") as out2:
-        for seq1, seq2 in final_paired_reads:
-            out1.write(f"{seq1}\n".encode())
-            out2.write(f"{seq2}\n".encode())
 
-    with open(output_file1, 'rb') as f_in, pyzstd.open(output_file1 + '.zst', 'wb') as f_out:
-        f_out.write(f_in.read())
+    output_files = [file.replace(".fastq.gz", ".reads") for file in files]
+    
+    with open(output_files[0], "wb") as out1:
+        for read_tuple in final_paired_reads:
+            out1.write(f"{read_tuple[0]}\n".encode())
+            
+    for i, output_file in enumerate(output_files[1:], start=1):
+        with open(output_file, "wb") as out:
+            for read_tuple in final_paired_reads:
+                out.write(f"{read_tuple[i]}\n".encode())
+                
+        compress_and_write(output_file)
+        
+    compress_and_write(output_files[0])
 
-    with open(output_file2, 'rb') as f_in, pyzstd.open(output_file2 + '.zst', 'wb') as f_out:
-        f_out.write(f_in.read())
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python convert_to_reads.py file1.fastq.gz file2.fastq.gz")
+    if len(sys.argv) < 2:
+        print("Usage: python convert_to_reads.py file1.fastq.gz [file2.fastq.gz ...]")
         sys.exit(1)
 
-    file1 = sys.argv[1]
-    file2 = sys.argv[2]
-
-    read_and_process(file1, file2)
+    files = sys.argv[1:]
+    read_and_process(*files)
