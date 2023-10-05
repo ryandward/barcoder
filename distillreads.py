9683b6a32a3c3bbed630ad1c0263d98dffe41855
Ryan Ward <ryandward@gmail.com>
2023-10-05 16:13:32 -0500

    fix multithreader

--- distillreads.py
+++ distillreads.py
@@ -13,7 +13,7 @@ def read_file(filename, queue, barrier, read_complete_event):
             count += 1
             if count % 4 == 2:
                 sequences.append(line.strip())
-                if len(sequences) == 100000:
+                if len(sequences) == 65536*4:
                     queue.put(sequences.copy())
                     print(f"Reader {multiprocessing.current_process().name} completed reading chunk of size: ", len(sequences))
                     sequences.clear()
@@ -28,7 +28,7 @@ def read_file(filename, queue, barrier, read_complete_event):
     print(f"Reader {multiprocessing.current_process().name} completed all reads, shutting down.")
 
 
-def sorter(input_queues, sorted_chunks_queue, read_complete_events, sort_complete_event):
+def sorter(input_queues, sorted_chunks_queue, read_complete_events, sort_complete_event, sorter_lock):
     print(f"Sorter {multiprocessing.current_process().name} started.")
     while True:
         all_queues_empty = all([q.empty() for q in input_queues])
@@ -53,14 +53,16 @@ def sorter(input_queues, sorted_chunks_queue, read_complete_events, sort_complet
         # Check if all readers are done and all queues are empty
         if all([event.is_set() for event in read_complete_events]) and all([q.empty() for q in input_queues]):
             break
-
+        
+        chunks = []
         # Only proceed if all the queues are not empty
-        if all([not q.empty() for q in input_queues]):
-            # Get chunks from all queues to create tuples
-            chunks = [q.get() for q in input_queues]
+        with sorter_lock:  # Acquire the lock to ensure exclusive access
+            if all([not q.empty() for q in input_queues]):
+                # Get chunks from all queues to create tuples
+                chunks = [q.get() for q in input_queues]
+                print(f'Sorter {multiprocessing.current_process().name} got chunks of size: ', [len(chunk) for chunk in chunks]) 
 
-            print(f'Sorter {multiprocessing.current_process().name} got chunks of size: ', [len(chunk) for chunk in chunks]) 
-                  
+                 
             # Create tuples for sorting
             tuples_to_sort = list(zip(*chunks))
 
@@ -105,10 +107,10 @@ def k_way_merge(chunks):
     
     return merged
 
-def merger(sorted_chunks_queue, output_queues, sort_complete_events):
-    # Wait for all sorters to complete their tasks
-    # while not all([event.is_set() for event in sort_complete_events]):
-    #     pass
+def merger(sorted_chunks_queue, output_queues, sort_complete_events, merger_done_event):
+        # Wait for all sorters to complete their tasks
+    while not all([event.is_set() for event in sort_complete_events]):
+        pass
 
     all_chunks = []
 
@@ -118,14 +120,14 @@ def merger(sorted_chunks_queue, output_queues, sort_complete_events):
     while end_signals_received < len(sort_complete_events):
         chunk = sorted_chunks_queue.get()
         print(f"end_signals_received: {end_signals_received}", len(sort_complete_events))
+        
         # Check for end signal
         if chunk is None:
             end_signals_received += 1
-            continue
-        
-        print('Merger got chunk of size: ', len(chunk))
-        all_chunks.append(chunk)
-        print(f'Merger has {len(all_chunks)} chunks in total')
+        elif len(chunk) > 0:
+            print('Merger got chunk of size: ', len(chunk))
+            all_chunks.append(chunk)
+            print(f'Merger has {len(all_chunks)} chunks in total')
 
     print('Merger got all chunks, starting k-way merge of size: ', len(all_chunks))
     merged_list = k_way_merge(all_chunks)
@@ -139,9 +141,15 @@ def merger(sorted_chunks_queue, output_queues, sort_complete_events):
     # Signal completion to writers
     for q in output_queues:
         q.put(None)
+    
+    merger_done_event.set()
 
 
-def write_output(queue, filename, read_complete_event):
+
+def write_output(queue, filename, merger_done_event):
+    # Wait for the merger to finish
+    merger_done_event.wait()
+    
     sequences = []
     while True:
         chunk = queue.get()
@@ -153,11 +161,15 @@ def write_output(queue, filename, read_complete_event):
         for seq in sequences:
             f.write(seq + '\n')
 
+
 if __name__ == "__main__":
-    num_sorters = 8  # Or any other number based on available resources
+    num_sorters = 100  # Or any other number based on available resources
 
     filenames = sys.argv[1:]
     manager = multiprocessing.Manager()
+    sorter_lock = manager.Lock()
+    merger_done_event = manager.Event()
+
 
     queues = [manager.Queue() for _ in filenames]
     chunk_complete_events = [multiprocessing.Event() for _ in filenames]
@@ -170,11 +182,11 @@ if __name__ == "__main__":
     sorted_chunks_queue = manager.Queue()
 
 
-    merger_process = multiprocessing.Process(target=merger, args=(sorted_chunks_queue, output_queues, sort_complete_events))
+    merger_process = multiprocessing.Process(target=merger, args=(sorted_chunks_queue, output_queues, sort_complete_events, merger_done_event))
     barrier = multiprocessing.Barrier(len(filenames))  # One barrier point for each reader
     readers = [multiprocessing.Process(target=read_file, args=(filename, queue, barrier, event)) for filename, queue, event in zip(filenames, queues, read_complete_events)]
-    sorters = [multiprocessing.Process(target=sorter, args=(queues, sorted_chunks_queue, read_complete_events, sort_complete_events[i])) for i in range(num_sorters)]
-    writers = [multiprocessing.Process(target=write_output, args=(queue, filename, event)) for queue, filename, event in zip(output_queues, filenames, read_complete_events)]
+    sorters = [multiprocessing.Process(target=sorter, args=(queues, sorted_chunks_queue, read_complete_events, sort_complete_events[i], sorter_lock)) for i in range(num_sorters)]
+    writers = [multiprocessing.Process(target=write_output, args=(queue, filename, merger_done_event)) for queue, filename in zip(output_queues, filenames)]
 
     for r in readers:
         r.start()
