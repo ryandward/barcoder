195c2004274b1725bb173ead6c1272f7d7842737
Ryan David Ward <ryandward@gmail.com>
2024-03-06 23:40:24 -0600

    added docs

--- CRISPRiLibrary.py
+++ CRISPRiLibrary.py
@@ -18,6 +18,19 @@ class CRISPRiLibrary:
         )
 
     def get_source_unique_targets(self):
+        """
+        Returns a DataFrame containing the unique source targets.
+
+        The source targets are objects that contain information that is completely agnostic to features such as genes.
+        Instead, they only contain a locus map for each chromosome.
+        This method filters the targets based on their type, targeting status, and mapping status.
+        The resulting DataFrame contains only the unique source targets.
+
+        Note: The term "source" comes from NCBI GenBank annotations, where "source" refers to the chromosomal coordinate information.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the unique source targets.
+        """
         return (
             self.targets_df[
                 (self.targets_df["Type"] == "source")
@@ -29,6 +42,19 @@ class CRISPRiLibrary:
         )
 
     def get_mapped_targets(self):
+        """
+        Returns a DataFrame containing the mapped targets.
+
+        This method filters the targets DataFrame to include only the targets that meet the following criteria:
+        - The target type is not "source", i.e. it is a gene or other feature
+        - The target is marked as targeting
+        - The target is marked as mapped
+
+        The method then calculates the offset and overlap for each target and adds them as new columns to the DataFrame.
+
+        Returns:
+            pandas.DataFrame: DataFrame containing the mapped targets with additional columns for offset and overlap.
+        """
         return (
             self.targets_df[
                 (self.targets_df["Type"] != "source")
@@ -54,6 +80,15 @@ class CRISPRiLibrary:
         )
 
     def _get_unique_targets(self):
+        """
+        Returns a list of unique guide targets based on their location in the genome.
+
+        This method filters the mapped targets based on their barcode and sorts them by chromosome, start, and end positions.
+        The resulting list contains only the targets that are unique with respect to their location in the genome.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the unique guide targets.
+        """
         mapped_targets = self.get_mapped_targets()
         return (
             mapped_targets[
@@ -64,6 +99,19 @@ class CRISPRiLibrary:
         )
 
     def _get_unambiguous_targets(self):
+        """
+        Returns a list of unambiguous guide targets based on their barcode.
+
+        This method filters the unique targets based on their barcode and removes any duplicates.
+        The resulting list contains only the targets that are unambiguous with respect to their barcode.
+
+        Note: This method is probably excessively stringent, as it does not allow for any overlapping genes,
+        which are often biologically relevant. Performing the unambiguous option will only make post-experimental
+        deconvolution easier but at the potential expense of biologically meaningful information.
+
+        Returns:
+            pandas.DataFrame: A DataFrame containing the unambiguous guide targets.
+        """
         return self.unique_targets[
             ~self.unique_targets.duplicated(subset=["Barcode"]).reset_index(drop=True)
         ]
